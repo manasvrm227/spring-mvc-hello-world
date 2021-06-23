d665eddafda6eea88609f6bd2a278fd312f03b02
manasvrm227 <manasvrm227@gmail.com>
2021-06-23 21:38:57 +0530

    initial commit

---
+++ src/main/java/com/hello/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping
+	public String display() {
+		return "index";
+	}
+}
